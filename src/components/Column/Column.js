831438667c30d7d3e034e834997dc0e3284e1682
Jakub Firlej <jakub.firlej@outlook.com>
2019-10-14 20:15:13 +0200

    vreate compontes columnt

---
+++ src/components/Column/Column.js
@@ -0,0 +1,13 @@
+import React from "react";
+import styles from "./Column.scss";
+
+class Column extends React.Component {
+  render() {
+    return (
+      <section className={styles.componet}>
+        <h3 className={styles.title}></h3>
+      </section>
+    );
+  }
+}
+export default Column;
